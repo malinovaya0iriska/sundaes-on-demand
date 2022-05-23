60e656ef67398722ab49d2c8acee589aa1281c2c
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-23 12:43:06 +0300

    test alert banner if server error occured

--- src/pages/common/AlertBanner.jsx
+++ src/pages/common/AlertBanner.jsx
@@ -2,7 +2,7 @@ import Alert from 'react-bootstrap/Alert';
 
 export const AlertBanner = ({ message, variant }) => {
   const alertMessage =
-    message || 'An unexpected error ocured. Please try again later.';
+    message || 'An unexpected error occurred. Please try again later.';
   const alertVariant = variant || 'danger';
 
   return (
