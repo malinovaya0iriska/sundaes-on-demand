4bf7af6a4d583a415d60320390bd9988251946e3
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-23 11:42:30 +0300

    code and test for the input validating

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import { mockURL } from './../../mocks/api';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 
 export const ScoopOption = ({ name, imagePath, updateItemCount }) => {
+  const [isValid, setIsValid] = useState(true);
+
   const handleChange = (event) => {
-    updateItemCount(name, event.target.value);
+    const currentValue = event.target.value;
+
+    // make sure the value is a number and not a string to validate
+    const currentValueFloat = parseFloat(currentValue);
+    const valueIsValid =
+      0 <= currentValueFloat &&
+      currentValueFloat <= 10 &&
+      Math.floor(currentValueFloat) === currentValueFloat;
+
+    // validate
+    setIsValid(valueIsValid);
+
+    // only update context if the value is valid
+    if (valueIsValid) updateItemCount(name, currentValue);
   };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ texalign: 'center' }}>
@@ -27,6 +43,7 @@ export const ScoopOption = ({ name, imagePath, updateItemCount }) => {
             type='number'
             defaultValue={0}
             onChange={handleChange}
+            isInvalid={!isValid}
           ></Form.Control>
         </Col>
       </Form.Group>
