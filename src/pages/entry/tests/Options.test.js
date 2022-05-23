6a870f5f6a466895edae5327df6551e54b15f819
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-23 12:13:56 +0300

    test no updating of total of scoops  via invalid input

--- src/pages/entry/tests/Options.test.js
+++ src/pages/entry/tests/Options.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils/testing-library-utils';
+import userEvent from '@testing-library/user-event';
 import { ENDPOINTS } from '../../../mocks/api';
 import { Options } from '../Options';
 import { OrderDetailsProvider } from '../../../context/OrderDetailes';
@@ -31,4 +32,17 @@ test('displays image for each topping option from server', async () => {
     'M&Ms topping',
     'Hot fudge topping',
   ]);
+});
+
+test("don't update total if scoops input is invalid", async () => {
+  render(<Options optionType={ENDPOINTS.SCOOPS} />);
+
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '-1');
+
+  const scoopsSubtotal = screen.getByText('Scoops total: $0.00');
+  expect(scoopsSubtotal).toBeInTheDocument();
 });
