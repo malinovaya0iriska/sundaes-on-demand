04e84947bfc1da917842d2fe479f1765c7275d6a
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-23 11:05:53 +0300

    test and code disabling of button

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -2,6 +2,8 @@ import { screen, waitFor } from '@testing-library/react';
 import { OrderEntry } from '../OrderEntry';
 import { render } from '../../../test-utils/testing-library-utils';
 import { rest } from 'msw';
+import userEvent from '@testing-library/user-event';
+
 import { server } from '../../../mocks/server';
 import { mockURL, ENDPOINTS } from '../../../mocks/api';
 
@@ -24,3 +26,20 @@ test('handle error for scoops and toppings routes', async () => {
 });
 
 // test.skip('test that is skipped', () => {});
+
+test('disable order button if there are no scoops ordered', async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+  const orderButton = screen.getByRole('button', { name: /order/i });
+  expect(orderButton).toBeDisabled();
+
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, '1');
+  expect(orderButton).toBeEnabled();
+
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, '0');
+  expect(orderButton).toBeDisabled();
+});
